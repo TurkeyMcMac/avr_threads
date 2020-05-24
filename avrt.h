f688b8bf0b3817fb453ab7b32f20227f026de129
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-05-24 09:59:21 -0400

    Add header guard

--- avrt.h
+++ avrt.h
@@ -1,3 +1,6 @@
+#ifndef AVR_THREADS_H_
+#define AVR_THREADS_H_
+
 unsigned char avrt_init(void);
 
 signed char avrt_start(void *arg, void (*func)(void *arg), void *stack);
@@ -5,3 +8,5 @@ signed char avrt_start(void *arg, void (*func)(void *arg), void *stack);
 void avrt_yield(void);
 
 void avrt_exit(void) __attribute__((noreturn));
+
+#endif /* AVR_THREADS_H_ */
