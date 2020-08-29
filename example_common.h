7fbff6ed206f4834eb29322dc0171b744d966214
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2020-08-29 11:42:22 -0400

    Allow unused example functions

--- example_common.h
+++ example_common.h
@@ -7,14 +7,14 @@
 #include <avr/io.h>
 
 /* Initialize PB3, PB4, and PB5 for misc. use. */
-static void ports_init(void)
+__attribute__((unused)) static void ports_init(void)
 {
 	DDRB |= _BV(PB3) | _BV(PB4) | _BV(PB5);
 }
 
 /* Initialize TIMER0 (assumed to be 8-but) as the thread timer with a reasonable
  * interval, without enabling interrupts. */
-static void thread_timer_init(void)
+__attribute__((unused)) static void thread_timer_init(void)
 {
 	TCCR0B = _BV(CS02);
 	TIMSK0 = _BV(TOIE0);
